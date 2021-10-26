7fca04c349bd76a8f677c6daee72a232b89b7c67
apsa <pauloserpaantunes@gmail.com>
2021-10-26 20:22:20 -0300

    :heavy_plus_sign: Devise instalation with model and views for Admins, Professionals and Clients

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :clients
+  devise_for :professionals
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
