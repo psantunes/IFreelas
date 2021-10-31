064eac033555cbafca3b766a4768c297df882ed7
apsa <pauloserpaantunes@gmail.com>
2021-10-31 11:33:06 -0300

    :heavy_plus_sign: Install ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :clients
   devise_for :professionals
-  devise_for :admins
+  devise_for :admins, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create(name: 'admin', email: 'admin@admin.com', password: '123456')
+Admin.create(name: 'admin', email: 'admin@admin.com', password: '123456') if Rails.env.development?
