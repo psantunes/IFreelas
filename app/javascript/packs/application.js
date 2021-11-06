e4a68d0bcaf57a03ec40f2c482df5416ebd6c9c1
apsa <pauloserpaantunes@gmail.com>
2021-11-06 18:29:14 -0300

    :arrow_down: Adjust versions of JQuery, Popper and Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "bootstrap"
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,5 +13,3 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-import "jquery"
