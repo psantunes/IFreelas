fa7d8c1ec8bf6c25197ead8cefa92a7360ec37d6
apsa <pauloserpaantunes@gmail.com>
2021-11-06 16:04:40 -0300

    :bento: Create view and controller for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :clients
   devise_for :professionals
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+
+  get '/home', to: 'home#index'
 end
