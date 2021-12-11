4fd2e0e468dbb35fbd0b6bd083fb92cf7a057ce7
apsa <pauloserpaantunes@gmail.com>
2021-12-11 18:01:39 -0300

    :truck: Change gem for open e-mails in localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,11 +75,9 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Devise config
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.asset_host = 'http://localhost:3000'
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.asset_host = 'http://localhost:3000'
 
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
-  config.action_mailer.default_options = { from: ENV['SMTP_FROM'] }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
