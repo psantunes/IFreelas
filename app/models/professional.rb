b8e366771b9c7436a96ab097d94e0c828e465429
apsa <pauloserpaantunes@gmail.com>
2021-12-12 17:02:51 -0300

    :sparkles: Upload avatars

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -11,6 +11,8 @@ class Professional < ApplicationRecord
   has_and_belongs_to_many :skills
   accepts_nested_attributes_for :experiences, allow_destroy: true
 
+  has_one_attached :avatar
+
   enum semester: { "1º": 1, "2º": 2, "3º": 3, "4º": 4, "5º": 5, "6º": 6 }
   enum type_of_work: { "In company": 0, "Home office": 1, Hìbrido: 2 }
   enum work_shift: { Manhã: 0, Tarde: 1, Noite: 2, Integral: 3, "Finais de semana": 4 }
