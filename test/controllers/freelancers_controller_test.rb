fb32e2007007c6438587689f6d090bbff1d00e7e
Antonio <as@cfgroup.com.br>
2021-12-30 10:21:41 -0300

    :fire: Remove default tests

--- test/controllers/freelancers_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class FreelancersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get freelancers_show_url
-    assert_response :success
-  end
-end
