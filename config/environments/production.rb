eb4e9e310954b099ffdfc9de6cc24de198d06c9a
Antonio <as@cfgroup.com.br>
2022-01-03 19:47:46 -0300

    :wrench: Configure mailer for production server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
@@ -63,6 +65,22 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ifreelas_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {
+    host: 'fierce-everglades-11253.herokuapp.com',
+    protocol: 'https'
+  }
+  config.action_mailer.asset_host = 'fierce-everglades-11253.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: ENV['SMTP_FROM'] }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
