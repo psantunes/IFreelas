42a8f4637692aec6f0b201847e73f4eed9a760c3
Antonio <as@cfgroup.com.br>
2022-01-09 10:42:41 -0300

    :green_heart: Add mailer variables to pass Rspec inspection in Github Actions

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,19 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   Rails.application.routes.default_url_options[:host] = 'http://localhost:3000'
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.asset_host = 'http://localhost:3000'
+
+  config.action_mailer.delivery_method = :smtp # :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_options = { from: ENV['SMTP_FROM'] }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
