54e0e2668c8a814249066c99aef4c70fd7640482
Antonio <as@cfgroup.com.br>
2022-01-09 00:25:56 -0300

    :bug: Fix bugs recommended by Rubocop in config folder

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 Dotenv::Railtie.load
 
 module Ifreelas
+  # Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,10 +1,8 @@
+# frozen_string_literal: true
+
 require 'active_support/core_ext/integer/time'
 
-# The test environment is used exclusively to run your application's
-# test suite. You never need to work with it otherwise. Remember that
-# your test database is "scratch space" for the test suite and is wiped
-# and recreated between test runs. Don't rely on the data there!
-
+# The test environment is used exclusively to run your application's test suite.
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
